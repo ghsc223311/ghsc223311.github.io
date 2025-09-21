5369351054e47dff6d8a48933583ff40bac128e3
ghsc223311 <1984leong@gmail.com>
2025-09-21 23:11:05 +0800

    Site updated: 2025-09-21 23:11:04

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/09/19/hello-world/","2025/09/21/如何发布文章到hexo博客上-含插入图片的方法/","2025/09/21/(WEB)2025 SWPU-NSSCTF 秋季招新入门训练赛/(WEB)2025 SWPU-NSSCTF 秋季招新入门训练赛/"];function toRandomPost(){
+var posts=["2025/09/21/WEB-2025-SWPU-NSSCTF-秋季招新入门训练赛/","2025/09/19/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };

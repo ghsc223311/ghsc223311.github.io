cbf2cf636682933caeaec95e0dbd91e31cb63ee2
ghsc223311 <1984leong@gmail.com>
2025-12-01 22:13:39 +0800

    Site updated: 2025-12-01 22:13:36

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["ghsc_ovo_0721/WEB-2025-SWPU-NSSCTF-秋季招新入门训练赛/","ghsc_ovo_0721/OpenSSH的使用/","ghsc_ovo_0721/NewStar2023-Canary/","ghsc_ovo_0721/Frida使用/","ghsc_ovo_0721/ROPgadget 使用/","ghsc_ovo_0721/dirsearch使用/"];function toRandomPost(){
+var posts=["ghsc_ovo_0721/WEB-2025-SWPU-NSSCTF-秋季招新入门训练赛/","ghsc_ovo_0721/MSF的使用/","ghsc_ovo_0721/OpenSSH的使用/","ghsc_ovo_0721/NewStar2023-Canary/","ghsc_ovo_0721/Frida使用/","ghsc_ovo_0721/dirsearch使用/","ghsc_ovo_0721/ROPgadget 使用/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
